93b277269d8a360e262caf01f2687a5bc9acd335
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-03 15:24:01 +0100

    Container Class 7 out of 9 Methods done.

---
+++ ChibuzoJavaAssignment/Kata.java
@@ -0,0 +1,110 @@
+public class Kata{
+
+
+static boolean isEven(int number1){
+
+int parity = number1%2;
+
+return parity==0;
+}
+
+
+
+
+static boolean isPrime(int number1){
+
+int factorCount=0;
+
+for(int count=2;count<=number1;count++){
+int factor = number1%count;
+
+if(factor==0){factorCount=factorCount+1;
+	     }
+					   }
+return factorCount<=1;
+}
+
+
+
+
+static int subract(int number1, int number2){
+if(number2<0) {
+	number2 = number2/-1;
+		   }
+int subract = number1 - number2;
+if(subract<0) {
+	subract = subract/-1;
+	      }
+return subract;
+}
+
+
+
+
+
+static float divide(int number1, int number2){
+if(number2==0){
+		return 0;
+		   }
+
+	int division = number1/number2;
+	return division;
+}
+
+
+
+
+
+static int factorOf(int number1){
+int factorCount=0;
+
+for(int count=1;count<=number1;count++){
+int factor=number1%count;
+if(factor==0){factorCount=factorCount+1;}
+
+					}
+return factorCount; }
+
+
+
+
+
+static boolean isSquare(int number2){
+
+double squareRoot = Math.sqrt(number2);
+
+return squareRoot*squareRoot== number2;
+}
+
+
+
+
+static boolean isPalindrome(int number2){
+int numfinal = number2;
+int rev = 0;
+
+if(number2>99 && number2<=999){
+
+while(number2>0){
+	rev = rev *10 + number2 % 10;
+	number2 = number2/10;
+	
+	}
+
+}
+return numfinal==rev;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
