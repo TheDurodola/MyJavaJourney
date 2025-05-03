ff299acc0a5b3209f53a1e0d1d26c3e99c995533
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-03 21:15:28 +0100

    Simplified the squareOf function
    Previously contained a for loop. Three lines of code could be made simplier.

--- ChibuzoJavaAssignment/Kata.java
+++ ChibuzoJavaAssignment/Kata.java
@@ -104,10 +104,8 @@ return factorial;
 }
 
 static long squareOf(int number1){
-int square = 1;
-for(int count =0;count<=2;count++){
-square = number1 * number1;}
 
+int square = number1 * number1;
 return square;
 
 }
