cb72b3b81c8726dbb38044d1a051d44099f463c8
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-04 12:23:39 +0100

    Change the first variable name in isLeapYear from check to checker1

--- FemiJavaAssignment/SundayMunchies.java
+++ FemiJavaAssignment/SundayMunchies.java
@@ -8,11 +8,11 @@ return fifthMultiple;
 
 static boolean isLeapYear(int year){
 
-int check = year%4;
+int checker1 = year%4;
 int checker2 = year%100;
 int checker3 = year%400;
 
-return check==0 && (checker2!=0 || checker3==0);
+return checker==0 && (checker2!=0 || checker3==0);
 
 
 
