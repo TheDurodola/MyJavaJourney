1b7bb13e484c4a05f7806fb1b982fb3d73b167c0
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-04 13:45:58 +0100

    Added the computeSumOfNumber function

--- FemiJavaAssignment/SundayMunchies.java
+++ FemiJavaAssignment/SundayMunchies.java
@@ -1,5 +1,7 @@
 public class SundayMunchies{
 
+
+
 static int findMultiple(int number){
 int fifthMultiple = number *5;
 return fifthMultiple;
@@ -12,12 +14,54 @@ int checker1 = year%4;
 int checker2 = year%100;
 int checker3 = year%400;
 
-return checker==0 && (checker2!=0 || checker3==0);
+return checker1==0 && (checker2!=0 || checker3==0);
+}
 
 
+
+/*
+static String displayAge(int ageYears){
+
+ageMonths = ageYears*12;
+ageDays =  ageYears*365;
+ageMinutes = ageDays*60*24;
+ageSeconds = ageMinutes*60;
+
+ageYears = Integer.toString(ageYears);
+ageMonths = Integer.toString(ageMonths);
+ageDays = Integer.toString(ageDays);
+ageMinutes = Integer.toString(ageMinutes);
+ageSeconds = Integer.toString(ageSeconds);
+
+return ageSeconds;
+
+}
+*/
+
+
+
+static int computeSumOfNumber(int number){
+ 
+int sumOfNumber =0;
+
+if(number>999 && number<10000){
+
+int sum = 0;
+for(int count=1000; count!=0; count/=10){
+int divide = number/count;
+sum = sum +divide;
+number = number%count;
 
 }
 
+sumOfNumber = sum;			}
+
+
+else{System.out.print("This is not a four digits number! ");}
+
+return sumOfNumber;
+
+}
 
 
 
