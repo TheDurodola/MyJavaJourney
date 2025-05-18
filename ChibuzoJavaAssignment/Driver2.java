f9efb55d5a15e3647f11700ea6b0eb460188bcf5
Durodola Abolaji <142089766+TheYRSD@users.noreply.github.com>
2025-05-18 13:08:48 +0100

    An improvement driver class for the ArrayKata Assignment

---
+++ ChibuzoJavaAssignment/Driver2.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+
+public class Driver2{
+public static void main(String[]args){
+
+
+int [] number = {23,56,45,2,7,56,89,23,24};
+
+System.out.println("The largest number in the array is "+ ArrayKata2.maximumIn(number));
+System.out.println("The smallest number in the array is "+ ArrayKata2.minimumIn(number));
+System.out.println("The sum of all numbers in the array is "+ ArrayKata2.sumOf(number));
+System.out.println("The sum of all even numbers in the array is "+ ArrayKata2.sumOfEvenNumbersIn(number));
+System.out.println("The sum of all odd numbers in the array is "+ ArrayKata2.sumOfOddNumbersIn(number));
+System.out.println("The smallest and largest numbers respectively in the array are "+ Arrays.toString(ArrayKata2.maximumAndMinimumOf(number)));
+System.out.println("The total number of odd numbers in the array is "+ (ArrayKata2.noOfOddNumbersIn(number)));
+System.out.println("The total number of even numbers in the array is "+ (ArrayKata2.noOfEvenNumbersIn(number)));
+System.out.println("The smallest and largest numbers respectively in the array are "+ Arrays.toString(ArrayKata2.maximumAndMinimumOf(number)));
+System.out.println("The smallest and largest numbers respectively in the array are "+ Arrays.toString(ArrayKata2.maximumAndMinimumOf(number)));
+System.out.println("The smallest and largest numbers respectively in the array are "+ Arrays.toString(ArrayKata2.maximumAndMinimumOf(number)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}}
