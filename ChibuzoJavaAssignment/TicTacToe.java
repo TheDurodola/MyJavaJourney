1a078777c09fc9e8f738d5d234de7d4eda454b65
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-05-25 16:11:22 +0100

    Game Board Completed

---
+++ ChibuzoJavaAssignment/TicTacToe.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TicTacToe{
+public static void main(String[]args){
+
+
+user
+
+
+char[][] board = {{' ','|',' ', '|'}, {'-','+','-', '+','-'}, {' ','|',' ', '|'},{'-','+','-', '+','-'}, {' ','|',' ', '|'},};
+
+ for (int rows = 0; rows < board.length; ++rows) {
+        for(int columns = 0; columns < board[rows].length; ++columns) {
+           System.out.print(board[rows][columns]);
+        }
+	System.out.println();
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}}
