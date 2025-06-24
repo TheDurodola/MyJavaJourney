968c94c63d3b566f44898ba2995fc3f0dcae4933
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-24 20:35:10 +0100

    Refactor Person class to manage problems with a static database and add functionality to change problem status

--- ProblemTracker/Person.java
+++ ProblemTracker/Person.java
@@ -6,30 +6,42 @@ import java.util.Scanner;
 
 
 public class Person {
+        private static List<Problem> database  = new ArrayList<>();
 
     public static void main(String[] args) {
-        List<Problem> database  = new ArrayList<>();
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the name of your problem: ");
+        System.out.println("Enter the name of your problem");
         String name = input.nextLine();
+        System.out.println("Enter the problem type");
+        ProblemType type = ProblemType.valueOf(input.nextLine());
+        addProblem(name, type);
 
-        System.out.println("\nPick a Problem Type;");
-        int index = 1;
-        for(ProblemType problemType : ProblemType.values()){
-            System.out.println(index + ". " + problemType);
-            index++;
+
+        System.out.println("Enter the name of the problem to change its status to resolved");
+        String problemName = input.nextLine();
+        changeStatus(problemName);
+    }
+
+    static void addProblem(String name, ProblemType type) {
+        database.add(new Problem(name,type));
+
+    }
+
+    static void changeStatus(String name) {
+        for (Problem problem : database) {
+            if (problem.getName().equals(name)) {
+                problem.setStatus(true);
+                System.out.println("Status changed to resolved for problem: " + name);
+                return;
+            }
         }
+        System.out.println("Problem not found: " + name);
 
-        System.out.print("\nType your problem type : ");
-        String type = input.nextLine();
-
-        ProblemType problemType = ProblemType.valueOf(type.toUpperCase());
-        Problem problem = new Problem(name, problemType);
-        database.add(problem);
-
-
-        System.out.println(database.get(0).toString());
     }
 
 }
+
+
+
+
