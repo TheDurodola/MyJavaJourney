3d60154596e06c1c17a45f158f93e5a6164df96a
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-26 13:56:07 +0100

    Add constructor to Product class for initializing fields

--- EStore/src/Product.java
+++ EStore/src/Product.java
@@ -4,4 +4,12 @@ public class Product {
     private int productPrice;
     private String productDescription;
     private ProductCategory productCategory;
+
+    public Product(String productID, String productName, int productPrice, String productDescription, ProductCategory productCategory) {
+        this.productID = productID;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productDescription = productDescription;
+        this.productCategory = productCategory;
+    }
 }
