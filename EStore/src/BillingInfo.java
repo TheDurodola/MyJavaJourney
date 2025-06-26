94d629e24c3c1161e1082b7a51ac91d128267e67
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-26 13:55:21 +0100

    Add constructor to BillingInfo class for initializing fields

--- EStore/src/BillingInfo.java
+++ EStore/src/BillingInfo.java
@@ -2,4 +2,10 @@ public class BillingInfo {
     private String receiverPhoneNumber;
     private String receiverName;
     private Addresses deliveryAddress;
+
+    public BillingInfo(String receiverPhoneNumber, String receiverName, Addresses deliveryAddress) {
+        this.receiverPhoneNumber = receiverPhoneNumber;
+        this.receiverName = receiverName;
+        this.deliveryAddress = deliveryAddress;
+    }
 }
