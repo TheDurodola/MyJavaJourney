882fb3720d95ca1e2bc8001362bd58dd030e0588
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-26 13:55:10 +0100

    Add constructor to Addresses class for initializing all fields

--- EStore/src/Addresses.java
+++ EStore/src/Addresses.java
@@ -4,4 +4,12 @@ public class Addresses {
     private String houseNumber;
     private String streetName;
     private String stateName;
+
+    public Addresses(String cityName, String countryName, String houseNumber, String streetName, String stateName) {
+        this.cityName = cityName;
+        this.countryName = countryName;
+        this.houseNumber = houseNumber;
+        this.streetName = streetName;
+        this.stateName = stateName;
+    }
 }
