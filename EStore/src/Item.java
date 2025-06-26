57e94718484570ad74f72d8c042d8da3632642e2
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-26 13:55:54 +0100

    Add constructor to Item class for initializing quantity and product items

--- EStore/src/Item.java
+++ EStore/src/Item.java
@@ -2,4 +2,8 @@ public class Item {
     private String quantity;
     private Product productItems;
 
+    public Item(String quantity, Product productItems) {
+        this.quantity = quantity;
+        this.productItems = productItems;
+    }
 }
