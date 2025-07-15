2e01d0dfae1b8e2ad1bee51af1ffefc7b7bdc036
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-15 06:29:13 +0100

    isLocked, lockDiary, unlockDiary Implemented

--- S2/diaresApp/src/Diary.java
+++ S2/diaresApp/src/Diary.java
@@ -1,7 +1,23 @@
 public class Diary {
+    private String username;
+    private String password;
+    private boolean isLocked;
 
     public Diary(String username, String password) {
-
+    this.username = username;
+    this.password = password;
     }
 
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void lockDiary() {
+        isLocked = true;
+    }
+
+    public void unlockDiary() {
+        isLocked = false;
+    }
 }
