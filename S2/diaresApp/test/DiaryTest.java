bcca61b217080ce3c0f887807e8753b9d0f64c21
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-15 06:06:32 +0100

    Create DiaryTest.java

---
+++ S2/diaresApp/test/DiaryTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DiaryTest {
+
+    @BeforeEach
+    public void setup(){
+        Diary diary = new Diary(String username, String password);
+    }
+
+    @Test
+    public void (){}
+
+}
