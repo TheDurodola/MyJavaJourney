4c5b342159773b3e0be4ae4515d705b51160200f
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-15 20:29:15 +0100

    Update Diary.java

--- S2/diaresApp/src/Diary.java
+++ S2/diaresApp/src/Diary.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Diary {
     private String username;
     private String password;
     private boolean isLocked;
+    private int idCounter;
+    private List<Entry> entries = new ArrayList<>();
+
 
     public Diary(String username, String password) {
     this.username = username;
@@ -17,7 +23,33 @@ public class Diary {
         isLocked = true;
     }
 
-    public void unlockDiary() {
-        isLocked = false;
+    public void unlockDiary(String password) {
+        validatePassword(password);
+            isLocked = false;
+    }
+
+    private void validatePassword(String password) {
+        if (!this.password.equals(password)) {
+            throw new IncorrectPasswordException();
+        }
+    }
+
+    public void createEntry(String title, String description) {
+        idCounter++;
+        Entry newEntry = new Entry(idCounter, title, description);
+        entries.add(newEntry);
+    }
+
+    public Entry findEntryById(int id) {
+
+        for (Entry entry : entries) {
+            if (id == entry.getID())
+                return entry;
+        }
+        throw new InvalidIDException();
+    }
+
+    public void deleteEntry(int ID) {
+        entries.removeIf(entry -> entry.getID() == ID);
     }
 }
