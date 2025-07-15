7bda61264db947a96a3242b8ad99fd263bfb3ee8
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-15 06:28:34 +0100

    Three TestCase Passed

--- S2/diaresApp/test/DiaryTest.java
+++ S2/diaresApp/test/DiaryTest.java
@@ -1,14 +1,38 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DiaryTest {
+    Diary diary;
 
     @BeforeEach
-    public void setup(){
-        Diary diary = new Diary(String username, String password);
+    public void setUp(){
+        diary = new Diary("boluwatife", "Money$12");
+    }
+
+
+
+    @Test
+    public void isLockedIsFalseWhenDiaryIsFirstInstantiated(){
+
+        assertFalse(diary.isLocked());
     }
 
     @Test
-    public void (){}
+    public void isLockedIsTrueWhenLockDiaryIsUsed(){
+        diary.lockDiary();
+        assertTrue(diary.isLocked());
+    }
+
+    @Test
+    public void isLockedIsFalseAfterUnlockedDiaryIsUsedOnALockedDiary(){
+        diary.lockDiary();
+        assertTrue(diary.isLocked());
+        diary.unlockDiary();
+        assertFalse(diary.isLocked());
+
+    }
 
 }
