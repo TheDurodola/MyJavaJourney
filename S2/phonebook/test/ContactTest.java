8cd1dea841c9d7323ba82e8f00c24ddf9ba414b7
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-19 18:55:38 +0100

    Create ContactTest.java

---
+++ S2/phonebook/test/ContactTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.BeforeEach;
+
+public class ContactTest {
+
+    @BeforeEach
+    public void setUp() {
+        Contact contact = new Contact(1 , Abolaji, Durodola, 08148260470);
+    }
+}
