b56442e65778e34a04cb405a953b06cd91cb2204
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-20 20:16:30 +0100

    Update MainApplication.java

--- S2/phonebook/src/MainApplication.java
+++ S2/phonebook/src/MainApplication.java
@@ -5,8 +5,8 @@ public class MainApplication {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Phonebook phonebook = new Phonebook();
-
-        while (true) {
+        boolean loopCondition = true;
+        while (loopCondition) {
             String mainMenu = """
                     \t\tPhonebook
                     1) Add Contact
@@ -23,12 +23,17 @@ public class MainApplication {
                 case 1:
                 {
                     System.out.println("Enter first name:");
-                    String firstName = input.nextLine();
+                    String firstName = input.next();
                     System.out.println("Enter last name:");
-                    String lastName = input.nextLine();
+                    String lastName = input.next();
                     System.out.println("Enter phone number:");
-                    String phoneNumber = input.nextLine();
-                    phonebook.addContact(firstName, lastName, phoneNumber);
+                    String phoneNumber = input.next();
+                    try {
+                        phonebook.addContact(firstName, lastName, phoneNumber);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+
                     break;
                 }
 
@@ -36,17 +41,56 @@ public class MainApplication {
                 case 2:
                 {
                     System.out.println("Enter contact first name, last name, or phone number:");
-                    String userInput = input.nextLine();
-                    System.out.println(phonebook.findContact(userInput));
+                    String userInput = input.next();
+                    try {
+                        System.out.println(phonebook.findContact(userInput));
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 }
 
                 case 3:
-                {
-                    System.out.println("Enter contact first name, last name, and phone number:");
-                    String userInput = input.nextLine();
-                    
-                }
+                    {
+                    System.out.println("Enter contact first name:");
+                    String firstName = input.next();
+                    System.out.println("Enter contact last name:");
+                    String lastName = input.next();
+                    System.out.println("Enter contact phone number:");
+                    String phoneNumber = input.next();
+                    try {
+                        phonebook.deletedContact(firstName,  lastName, phoneNumber);
+                    } catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                    }
+                case 4:
+                    {
+                    System.out.println("Enter contact ID:");
+                    int contactID = input.nextInt();
+                    System.out.println("Enter new first name:");
+                    String firstName = input.next();
+                    System.out.println("Enter new last name:");
+                    String lastName = input.next();
+                    System.out.println("Enter new phone number:");
+                    String phoneNumber = input.next();
+                    try {
+                        phonebook.editContact(contactID, firstName, lastName, phoneNumber);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                    }
+
+                case 0:
+                    loopCondition = false;
+
+                default:
+                    {
+                        System.out.println("Invalid input!");
+                        break;
+                    }
             }
 
 
