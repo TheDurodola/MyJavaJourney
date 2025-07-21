8a1442a0665c4e88ee733007f4de379a303457c4
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-21 21:53:20 +0100

    Create IllegalNameException.java

---
+++ S2/phonebook/src/IllegalNameException.java
@@ -0,0 +1,9 @@
+public class IllegalNameException extends IllegalArgumentException{
+
+    public IllegalNameException(){
+
+    }
+    public  IllegalNameException(String s){
+        super(s);
+    }
+}
