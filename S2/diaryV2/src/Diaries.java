bd867c1ff06c67ae9a664306705b390a9cdbc894
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-25 06:30:32 +0100

    Implement diary entry management and password protection in Diaries

--- S2/diaryV2/src/Diaries.java
+++ S2/diaryV2/src/Diaries.java
@@ -9,22 +9,80 @@ public class Diaries {
         diaryV2.add(new DiaryV2(username, password));
     }
 
-    public DiaryV2 findDiary(String username) {
+    public DiaryV2 findDiary(String username, String password) {
         for (DiaryV2 diaryV2 : diaryV2) {
             if (username.equals(diaryV2.getUsername())) {
-                return diaryV2;
+                if (password.equals(diaryV2.getPassword())) {
+                    return diaryV2;
+                }
+                throw new IncorrectPasswordException();
             }
         }
-        return null;
+        throw new NoSuchDiaryException();
     }
 
-    public void delete(String username, String password) {
-        for (DiaryV2 diary : diaryV2) {
-            if (username.equals(diary.getUsername())) {
-                if (password.equals(diary.getPassword())){
-                    diaryV2.remove(diary);
+    public void deleteDiary(String username, String password) {
+        diaryV2.removeIf( diary -> username.equals(diary.getUsername()) && password.equals(diary.getPassword()));
+    }
+
+    public void createEntry(String username, String password, String title, String description) {
+        for(DiaryV2 diary : diaryV2) {
+            if(diary.getUsername().equals(username)) {
+                if(password.equals(diary.getPassword())) {
+                    diary.createEntry(title, description);
                 }
             }
         }
+
+    }
+
+    public void updateEntry(String username, String password, int entryId, String title, String description) {
+        for (DiaryV2 diaryV2 : diaryV2) {
+            if (username.equals(diaryV2.getUsername())) {
+                if (password.equals(diaryV2.getPassword())) {
+                    diaryV2.updateEntry(entryId, title, description);                }
+            }
+
+        }
+    }
+
+    public void deleteEntry(String username, String password, int entryId) {
+        for (DiaryV2 diaryV2 : diaryV2) {
+            if (username.equals(diaryV2.getUsername())) {
+                if (password.equals(diaryV2.getPassword())) {
+                    diaryV2.removeEntry(entryId);
+                }
+            }
+        }
+    }
+
+    public void lockDiary(String username) {
+        for(DiaryV2 diaryV2 : diaryV2) {
+            if(username.equals(diaryV2.getUsername())) {
+                    diaryV2.lockDiary();
+
+            }
+        }
+    }
+
+    public void unlockDiary(String username, String password) {
+        for(DiaryV2 diaryV2 : diaryV2) {
+            if(username.equals(diaryV2.getUsername())) {
+                if(password.equals(diaryV2.getPassword())) {
+                    diaryV2.unlockDiary(password);
+                }
+            }
+        }
+    }
+
+    public String printOutEntry(String username, String password, int entryId) {
+        for(DiaryV2 diaryV2 : diaryV2) {
+            if(username.equals(diaryV2.getUsername())) {
+                if(password.equals(diaryV2.getPassword())) {
+                    return diaryV2.printEntry(entryId);
+                }
+            }
+        }
+        throw new InvalidIDException();
     }
 }
